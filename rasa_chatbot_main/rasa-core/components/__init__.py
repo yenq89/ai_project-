cef256c8cf82b5e32c30d3f39d2db0fd99751708
yenq89 <ngohaiyen_t67@hus.edu.vn>
2024-12-03 02:26:40 +0700

    Đã config được UndertheSea

--- rasa_chatbot_main/rasa-core/components/__init__.py
+++ rasa_chatbot_main/rasa-core/components/__init__.py
@@ -1,9 +1,9 @@
 # from rasa_nlu_examples.common import NotInstalled
 #
 # # Thử nhập khẩu UndertheseaTokenizer, nếu không tìm thấy thì sẽ thông báo lỗi và không tải
-# try:
-#     from rasa_nlu_examples.tokenizers.underthesea_tokenizer import UndertheseaTokenizer
-# except ImportError:
-#     UndertheseaTokenizer = NotInstalled("UndertheseaTokenizer", "underthesea")
-#
-# __all__ = ["UndertheseaTokenizer"]
+try:
+    from components.tokenizer import UndertheseaTokenizer
+except ImportError:
+    UndertheseaTokenizer = NotInstalled("UndertheseaTokenizer", "underthesea")
+
+__all__ = ["UndertheseaTokenizer"]
--- rasa_chatbot_main/rasa-core/components/tokenizer.py
+++ rasa_chatbot_main/rasa-core/components/tokenizer.py
@@ -1,38 +1,54 @@
-from underthesea import word_tokenize
-from rasa.nlu.tokenizers.tokenizer import Tokenizer
-from rasa.shared.nlu.training_data.message import Message
-from rasa.shared.nlu.constants import TEXT
-from rasa.engine.recipes.default_recipe import DefaultV1Recipe
-from rasa.engine.graph import GraphComponent
+from __future__ import annotations
+import logging
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
-from typing import Any, Dict, Optional
-
+from typing import Any, Dict, List, Text
+from rasa.nlu.tokenizers.tokenizer import Token, Tokenizer
+from rasa.shared.nlu.training_data.message import Message
+from rasa.engine.recipes.default_recipe import DefaultV1Recipe
+from rasa.engine.graph import ExecutionContext
+from rasa.nlu.constants import TOKENS_NAMES, MESSAGE_ATTRIBUTES
+from underthesea import word_tokenize
+logger = logging.getLogger(__name__)
 @DefaultV1Recipe.register(
-    [GraphComponent], is_trainable=False  # Đăng ký component trong pipeline của Rasa
+    DefaultV1Recipe.ComponentType.MESSAGE_TOKENIZER, is_trainable=False
 )
 class UndertheseaTokenizer(Tokenizer):
-    @classmethod
-    def create(
-        cls,
-        config: Dict[str, Any],
-        model_storage: ModelStorage,
-        resource: Resource,
-        execution_context: Any,
-    ) -> "UndertheseaTokenizer":
-        return cls(config)
+    provides = [TOKENS_NAMES[attribute] for attribute in MESSAGE_ATTRIBUTES]
 
-# class UndertheseaTokenizer(Tokenizer):
-#     def __init__(self, component_config=None):
-#         super().__init__(component_config)
+    # @classmethod
+    # def create(
+    #         cls,
+    #         config: Dict[Text, Any],
+    #         model_storage: ModelStorage,
+    #         resource: Resource,
+    #         execution_context: ExecutionContext,
+    # ) -> UndertheseaTokenizer:
+    #     return cls(config)
+    def __init__(self, component_config: Dict[Text, Any] = None) -> None:
+        super().__init__(component_config)
+        self.intent_tokenization_flag = component_config.get("intent_tokenization_flag", False)
+        self.intent_split_symbol = component_config.get("intent_split_symbol", "_")
 
-    # def tokenize(self, message, **kwargs):
-    #     # Sử dụng underthesea để tách từ
-    #     tokens = word_tokenize(message.text)
-    #     # Trả về danh sách các token (dưới dạng chuỗi)
-    #     return tokens
-
-    def tokenize(self, message: Message, attribute: str = TEXT):
+    def tokenize(self, message: Message, attribute: Text) -> List[Token]:
         text = message.get(attribute)
-        tokens = word_tokenize(text)
-        return self._convert_words_to_tokens(tokens, text)
+        words = word_tokenize(text)
+        tokens = self._convert_words_to_tokens(words, text)
+
+        if self.intent_tokenization_flag:
+            tokens = self._split_intent(tokens)
+
+        return tokens
+
+    def _split_intent(self, tokens: List[Token]) -> List[Token]:
+        split_tokens = []
+        for token in tokens:
+            if self.intent_split_symbol in token.text:
+                split_tokens.extend([
+                    Token(text, token.start + i)
+                    for i, text in enumerate(token.text.split(self.intent_split_symbol))
+                ])
+            else:
+                split_tokens.append(token)
+        return split_tokens
+
