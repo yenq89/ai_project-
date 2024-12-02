b7f2f9929369ee5de6ba101e968f892342d474d2
yenq89 <ngohaiyen_t67@hus.edu.vn>
2024-12-02 10:36:11 +0700

    Chưa fix được config thư viện underthesea

--- rasa_chatbot_main/rasa-core/components/tokenizer.py
+++ rasa_chatbot_main/rasa-core/components/tokenizer.py
@@ -1,12 +1,38 @@
-from rasa.nlu.tokenizers.tokenizer import Tokenizer
 from underthesea import word_tokenize
+from rasa.nlu.tokenizers.tokenizer import Tokenizer
+from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.constants import TEXT
+from rasa.engine.recipes.default_recipe import DefaultV1Recipe
+from rasa.engine.graph import GraphComponent
+from rasa.engine.storage.resource import Resource
+from rasa.engine.storage.storage import ModelStorage
+from typing import Any, Dict, Optional
 
-class UnderTheseaTokenizer(Tokenizer):
-    def __init__(self, component_config=None):
-        super().__init__(component_config)
+@DefaultV1Recipe.register(
+    [GraphComponent], is_trainable=False  # Đăng ký component trong pipeline của Rasa
+)
+class UndertheseaTokenizer(Tokenizer):
+    @classmethod
+    def create(
+        cls,
+        config: Dict[str, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: Any,
+    ) -> "UndertheseaTokenizer":
+        return cls(config)
 
-    def tokenize(self, message, **kwargs):
-        # Sử dụng underthesea để phân tách từ
-        tokens = word_tokenize(message.text)
-        # Trả về danh sách các token (dưới dạng chuỗi)
-        return tokens
+# class UndertheseaTokenizer(Tokenizer):
+#     def __init__(self, component_config=None):
+#         super().__init__(component_config)
+
+    # def tokenize(self, message, **kwargs):
+    #     # Sử dụng underthesea để tách từ
+    #     tokens = word_tokenize(message.text)
+    #     # Trả về danh sách các token (dưới dạng chuỗi)
+    #     return tokens
+
+    def tokenize(self, message: Message, attribute: str = TEXT):
+        text = message.get(attribute)
+        tokens = word_tokenize(text)
+        return self._convert_words_to_tokens(tokens, text)
